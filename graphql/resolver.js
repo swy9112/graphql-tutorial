72a51d836c182b56225abd5dcefb69e4e2d24e78
swy9112 <swy9112@naver.com>
2019-05-22 00:17:28 +0900

    파일업데이트

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,8 +1,10 @@
 import {getMovies} from "./db";
+import {getMovie} from "./db";
 
 const resolvers = {
     Query: {
-        movies: (_,{limit, rating}) => getMovies(limit, rating)
+        movies: (_,{limit, rating}) => getMovies(limit, rating),
+        movie: (_,{id}) => getMovie(id)
     }
 }
 
